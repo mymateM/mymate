2cfa542d65b3bdd3ae447c3552e46841fd43dc27
eunaj99 <esfjge@gmail.com>
2025-02-13 00:55:37 +0900

    refactor(MainHomeFragment): 데이터 세팅 함수 분리

--- MyMate/app/src/main/java/com/example/mymate/MainHomeFragment.kt
+++ MyMate/app/src/main/java/com/example/mymate/MainHomeFragment.kt
@@ -101,11 +101,11 @@ class MainHomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        comms(mainActivity)
+        callServer(mainActivity)
         resumed = "01"
     }
 
-    private fun comms(context: Context) {
+    private fun callServer(context: Context) {
         mainActivity = context as MainActivity
         var retrofit = RetrofitClientInstance.client
         var endpoint = retrofit?.create(getHomeInfo::class.java)
@@ -123,81 +123,14 @@ class MainHomeFragment : Fragment() {
                 if (response.isSuccessful) {
                     var household = response.body()!!.data.household
                     var me = response.body()!!.data.me
+                    val montBoldTypeface = Typeface.create(ResourcesCompat.getFont(mainActivity, R.font.montserrat_bold), Typeface.NORMAL)
+                    val suitBoldTypeface = Typeface.create(ResourcesCompat.getFont(mainActivity, R.font.suit_bold), Typeface.NORMAL) // 글씨체 적용을 위한 변수 선언
 
                     val ratio = household.by_now_expense.toInt() / household.by_previous_expense.toInt() // 지난 달 대비 현재 사용량
-                    var compareBigText = SpannableStringBuilder("지난 달 대비")
-                    binding.dDay.text = "정산일 D${household.settlement_d_day}" // 정산일 text 설정
-                    val expensetitle = digitprocessing(household.by_now_expense)
-                    binding.nownotitext.text = expensetitle
-                    val montBoldTypeface = Typeface.create(ResourcesCompat.getFont(mainActivity, R.font.montserrat_bold), Typeface.NORMAL)
-                    val suitBoldTypeface = Typeface.create(ResourcesCompat.getFont(mainActivity, R.font.suit_bold), Typeface.NORMAL)
-                    if (household.by_previous_expense.toInt() < household.by_now_expense.toInt()) {
-                        compareBigText = SpannableStringBuilder("지난 달 ${household.expense_duration}일간 대비 더 썼어요")
-                        binding.statusbilltext.setTextColor(ContextCompat.getColor(mainActivity, R.color.red_text))
-                        binding.statussubtextsmall.setTextColor(ContextCompat.getColor(mainActivity, R.color.red_text))
-                        binding.presentComparetop.isGone = false
-                        binding.presentComparebody.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.box_noradius))
-                        binding.presentguidetop.setGuidelinePercent(0.185f)
-                        binding.presentguidemid.setGuidelinePercent(0.365f)
-                        binding.compareicon.setImageResource(R.drawable.more)
-                    } else {
-                        compareBigText = SpannableStringBuilder("지난 달 ${household.expense_duration}일간 대비 덜 썼어요")
-                        binding.statusbilltext.setTextColor(ContextCompat.getColor(mainActivity, R.color.pie_green))
-                        binding.statussubtextsmall.setTextColor(ContextCompat.getColor(mainActivity, R.color.pie_green))
-                        binding.presentComparetop.isGone = true
-                        binding.compareicon.setImageResource(R.drawable.less)
-                        binding.presentComparebody.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.graph_hometop))
-                        binding.presentguidemid.setGuidelinePercent((0.365 + 0.235 * (household.by_now_expense.toFloat() / household.by_previous_expense.toFloat())).toFloat())
-                    }
-                    compareBigText.setSpan(ForegroundColorSpan(ContextCompat.getColor(mainActivity, R.color.purplevivid_buttonline)), compareBigText.length - 5, compareBigText.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                    binding.comparebigtxt.text = compareBigText
-                    binding.statusbilltext.text = digitprocessing(household.now_expense_diff.toInt().absoluteValue.toString())
-                    var spendpercent = SpannableStringBuilder("지금까지 예산의 ${household.by_now_budget_ratio}%를 썼어요")
-                    spendpercent.setSpan(ForegroundColorSpan(ContextCompat.getColor(mainActivity, R.color.purplevivid_buttonline)), 9, 9 + household.by_now_budget_ratio.length + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                    spendpercent.setSpan(TypefaceSpan(montBoldTypeface), 9, 9 + household.by_now_budget_ratio.length + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                    binding.spendnoti.text = spendpercent
-                    binding.spendgraphguide.setGuidelinePercent((0.06 + 0.88 * (household.by_now_budget_ratio.toFloat() / 100)).toFloat())
-                    binding.spendgraphpercent.text = "${household.by_now_budget_ratio}%"
-                    if (household.by_now_budget_ratio.toInt() > 100) {
-                        binding.spendgraphicguide.setGuidelinePercent(0.87f)
-                        binding.spendgraphguide.setGuidelinePercent(0.94f)
-                        binding.spendoverguide.setGuidelinePercent((0.94 - 0.88 * ((household.by_now_budget_ratio.toFloat() - 100f) / 100)).toFloat())
-                        binding.graphgraphichead.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.trangle_red))
-                        binding.spendgraphpercent.setBackgroundResource(R.drawable.box_radius8_red)
-                        spendpercent = SpannableStringBuilder("지금까지 예산을 ${household.by_now_budget_ratio.toInt() - 100}% 초과했어요")
-                        spendpercent.setSpan(ForegroundColorSpan(ContextCompat.getColor(mainActivity, R.color.purplevivid_buttonline)), 9, 9 + (household.by_now_budget_ratio.toInt() - 100).toString().length + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                        spendpercent.setSpan(TypefaceSpan(montBoldTypeface), 9, 9 + (household.by_now_budget_ratio.toInt() - 100).toString().length + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-                    } else if ((0.06 + 0.88 * (household.by_now_budget_ratio.toFloat() / 100)).toFloat() > 0.87f) {
-                        binding.spendgraphicguide.setGuidelinePercent(0.87f)
-                        binding.spendoverguide.setGuidelinePercent(0.94f)
-                        binding.graphgraphichead.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.trangle_purple))
-                        binding.spendgraphpercent.setBackgroundResource(R.drawable.box_radius8_purple)
-                    } else if ((0.06 + 0.88 * (household.by_now_budget_ratio.toFloat() / 100)).toFloat() < 0.125f) {
-                        binding.spendgraphicguide.setGuidelinePercent(0.125f)
-                        binding.spendoverguide.setGuidelinePercent(0.94f)
-                        binding.graphgraphichead.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.trangle_purple))
-                        binding.spendgraphpercent.setBackgroundResource(R.drawable.box_radius8_purple)
-                    } else {
-                        binding.spendgraphicguide.setGuidelinePercent((0.06 + 0.88 * (household.by_now_budget_ratio.toFloat() / 100)).toFloat())
-                        binding.spendoverguide.setGuidelinePercent(0.94f)
-                        binding.graphgraphichead.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.trangle_purple))
-                        binding.spendgraphpercent.setBackgroundResource(R.drawable.box_radius8_purple)
-                    }
-                    if (household.is_household_budget_over_warn) {
-                        binding.homestatustxt.text = "이대로라면 예산을 초과할 것 같아요"
-                    } else {
-                        binding.homestatustxt.text = "아주 잘 하고 있어요. 이대로만 유지하는 게 좋겠어요!"
-                    }
-                    val remainingbudget = SpannableStringBuilder("${digitprocessing(me.user_by_now_left_expense)}원")
-                    val totalbudget = SpannableStringBuilder("${digitprocessing(me.user_total_budget)}원")
-                    remainingbudget.setSpan(TypefaceSpan(suitBoldTypeface), remainingbudget.length - 1, remainingbudget.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                    totalbudget.setSpan(TypefaceSpan(suitBoldTypeface), totalbudget.length - 1, totalbudget.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                    binding.remainingbudget.text = remainingbudget
-                    binding.totalbudget.text = totalbudget
-                    val spentpercentfloat = (me.user_by_now_total_expense.toFloat() / me.user_total_budget.toFloat() * 100)
-                    val leftpercentfloat = (me.user_by_now_left_expense.toFloat() / me.user_total_budget.toFloat() * 100)
-                    pieapply(me.user_total_budget.toInt(), spentpercentfloat, leftpercentfloat, me.user_by_now_total_expense.toInt())
+                    setTopBoxData(household.settlement_d_day, household.by_now_expense)
+                    setUntilNowData(household.by_now_budget_ratio, household.is_household_budget_over_warn, montBoldTypeface)
+                    setPrevMonthCompareData(household.by_previous_expense, household.by_now_expense, household.now_expense_diff, household.expense_duration)
+                    setUserBudgetData(me.user_by_now_left_expense, me.user_total_budget, me.user_by_now_total_expense, suitBoldTypeface)
                     //TODO: 그래프 특수경우 대응
                 }
             }
@@ -225,10 +158,87 @@ class MainHomeFragment : Fragment() {
         })
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
+    private fun setTopBoxData(dDay: String, nowExpense: String) {
+        binding.dDay.text = "정산일 D${dDay}"
+        binding.nownotitext.text = digitprocessing(nowExpense)
+    }
+
+    private fun setUntilNowData(spendRatio: String, isBudgetExcessed: Boolean, montbold: Typeface) {
+        var spentPercent = SpannableStringBuilder("지금까지 예산의 ${spendRatio}를 썼어요")
+        spentPercent.setSpan(ForegroundColorSpan(ContextCompat.getColor(mainActivity, R.color.purplevivid_buttonline)), 9, 9 + spendRatio.length + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spentPercent.setSpan(TypefaceSpan(montbold), 9, 9 + spendRatio.length + 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.spendnoti.text = spentPercent
+        binding.spendgraphguide.setGuidelinePercent((0.06 + 0.88 * (spendRatio.toFloat() / 100)).toFloat())
+        binding.spendgraphpercent.text = "${spendRatio}%"
+        if (spendRatio.toInt() > 100) {
+            binding.graphgraphichead.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.trangle_red))
+            binding.spendgraphpercent.setBackgroundResource(R.drawable.box_radius8_red)
+            binding.spendgraphicguide.setGuidelinePercent(0.87f)
+            binding.spendgraphguide.setGuidelinePercent(0.94f)
+            binding.spendoverguide.setGuidelinePercent((0.94 - 0.88 *((spendRatio.toFloat() - 100f) / 100)).toFloat())
+            spentPercent = SpannableStringBuilder("지금까지 예산을 ${spendRatio.toInt() - 100}% 초과했어요")
+            spentPercent.setSpan(ForegroundColorSpan(ContextCompat.getColor(mainActivity, R.color.purplevivid_buttonline)), 9, 9 + (spendRatio.toInt() - 100).toString().length+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            spentPercent.setSpan(TypefaceSpan(montbold), 9, 9 + (spendRatio.toInt() - 100).toString().length+1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        } else {
+            binding.graphgraphichead.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.trangle_purple))
+            binding.spendgraphpercent.setBackgroundResource(R.drawable.box_radius8_purple)
+            if ((0.06 + 0.88 * (spendRatio.toFloat() / 100)).toFloat() > 0.87f) {
+                binding.spendgraphicguide.setGuidelinePercent(0.87f)
+                binding.spendoverguide.setGuidelinePercent(0.94f)
+            } else if ((0.06 + 0.88 * (spendRatio.toFloat() / 100)).toFloat() < 0.125f) {
+                binding.spendgraphicguide.setGuidelinePercent(0.125f)
+                binding.spendoverguide.setGuidelinePercent(0.94f)
+            } else {
+                binding.spendgraphicguide.setGuidelinePercent((0.06 + 0.88 * (spendRatio.toFloat() / 100)).toFloat())
+                binding.spendoverguide.setGuidelinePercent(0.94f)
+            }
+        }
+        if (isBudgetExcessed) {
+            binding.homestatustxt.text = "이대로라면 예산을 초과할 것 같아요"
+        } else {
+            binding.homestatustxt.text = "아주 잘 하고 있어요. 이대로만 유지하는 게 좋겠어요!"
+        }
+    }
+
+    private fun setPrevMonthCompareData(previousExpense: String, nowExpense: String, expenseDiff: String, expenseDuration: String) { // 지난달과의 비교 그래프 세팅
+        var compareResultTitle = SpannableStringBuilder("지난 달 대비")
+        if (previousExpense.toInt() < nowExpense.toInt()) {
+            compareResultTitle = SpannableStringBuilder("지난 달 ${expenseDuration}일간 대비 더 썼어요")
+            binding.statusbilltext.setTextColor(ContextCompat.getColor(mainActivity, R.color.red_text))
+            binding.statussubtextsmall.setTextColor(ContextCompat.getColor(mainActivity, R.color.red_text))
+            binding.presentComparetop.isGone = false
+            binding.presentComparebody.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.box_noradius))
+            binding.presentguidetop.setGuidelinePercent(0.185f)
+            binding.presentguidemid.setGuidelinePercent(0.365f)
+            binding.compareicon.setImageResource(R.drawable.more)
+        } else {
+            compareResultTitle = SpannableStringBuilder("지난 달 ${expenseDuration}일간 대비 덜 썼어요")
+            binding.statusbilltext.setTextColor(ContextCompat.getColor(mainActivity, R.color.pie_green))
+            binding.statussubtextsmall.setTextColor(ContextCompat.getColor(mainActivity, R.color.pie_green))
+            binding.presentComparetop.isGone = true
+            binding.compareicon.setImageResource(R.drawable.less)
+            binding.presentComparebody.setImageDrawable(ContextCompat.getDrawable(mainActivity, R.drawable.graph_hometop))
+            binding.presentguidemid.setGuidelinePercent((0.365 + 0.235 * (nowExpense.toFloat() / previousExpense.toFloat())).toFloat())
+        }
+        compareResultTitle.setSpan(ForegroundColorSpan(ContextCompat.getColor(mainActivity, R.color.purplevivid_buttonline)), compareResultTitle.length - 5, compareResultTitle.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.comparebigtxt.text = compareResultTitle
+        binding.statusbilltext.text = digitprocessing(expenseDiff.toInt().absoluteValue.toString())
+    }
+
+    private fun setUserBudgetData(leftExpense: String, totalBudget: String, spentBudget: String, suitbold: Typeface) {
+        val remainingbudget = SpannableStringBuilder("${digitprocessing(leftExpense)}원")
+        val totalbudget = SpannableStringBuilder("${digitprocessing(totalBudget)}원")
+        remainingbudget.setSpan(TypefaceSpan(suitbold), remainingbudget.length - 1, remainingbudget.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        totalbudget.setSpan(TypefaceSpan(suitbold), totalbudget.length - 1, totalbudget.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.remainingbudget.text = remainingbudget
+        binding.totalbudget.text = totalbudget
+        val spentpercentfloat = (spentBudget.toFloat() / totalBudget.toFloat() * 100)
+        val leftpercentfloat = (leftExpense.toFloat() / totalBudget.toFloat() * 100)
+        pieapply(totalBudget.toInt(), spentpercentfloat, leftpercentfloat, spentBudget.toInt())
+    }
+
     private fun pieapply(total: Int, now_left: Float, now_total: Float, realtotal: Int) {
         val montBoldTypeface = Typeface.create(ResourcesCompat.getFont(mainActivity, R.font.montserrat_bold), Typeface.NORMAL)
-        val suitMediumTypeface = Typeface.create(ResourcesCompat.getFont(mainActivity, R.font.suit_medium), Typeface.NORMAL)
         val suitBoldTypeface = Typeface.create(ResourcesCompat.getFont(mainActivity, R.font.suit_bold), Typeface.NORMAL)
         val suitSemiBoldTypeface = Typeface.create(ResourcesCompat.getFont(mainActivity, R.font.suit_semibold), Typeface.NORMAL)
         val piemidtext = SpannableStringBuilder("${digitprocessing(realtotal.toString())}원\n오늘까지 썼어요")
@@ -274,7 +284,7 @@ class MainHomeFragment : Fragment() {
         pieChart.invalidate()
     }
 
-    private fun digitprocessing(digits: String): String {
+    private fun digitprocessing(digits: String): String { // TODO: common 패키지로 뺄 것
         var textlength = digits.length
         var processed = ""
         while (0 < textlength) {
@@ -307,6 +317,6 @@ class MainHomeFragment : Fragment() {
         //TODO: refresh data
         super.onResume()
 
-        comms(mainActivity)
+        callServer(mainActivity)
     }
 }
